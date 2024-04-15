01c33fb8d0b9c5b49e2a2b42683d698a8c9a19dc
taras.prystaiko <tarasprystaiko@gmail.com>
2024-04-15 19:44:35 +0200

    GitProgectInit

--- PythonBasic/Hello World.py
+++ PythonBasic/Hello World.py
@@ -4,4 +4,5 @@ print("Hello, world")
 def power(num_1, num_2):     # get input value
     result = num_1 ** num_2  # perform calculation
     return result         		   # provide the result
+
 print("Hello, world")
