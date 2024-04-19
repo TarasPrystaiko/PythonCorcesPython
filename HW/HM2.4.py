5b23b419f0076f24b8df0e9978521dec186bb351
taras.prystaiko <tarasprystaiko@gmail.com>
2024-04-19 12:49:54 +0200

    t

--- HW/HM2.4.py
+++ HW/HM2.4.py
@@ -1,3 +1,8 @@
-a = input("Enter number")
-print(a)
-print(type(a))
+num1 = int(input("Enter first integer number: "))
+num2 = int(input("Enter second integer number: "))
+
+result1 = num1 + num2
+result2 = num1 * num2
+
+print(f" {num1} + {num2} = {result1}")
+print(f" {num1} * {num2} = {result2}")
