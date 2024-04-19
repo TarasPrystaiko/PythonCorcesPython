7d8f55185ae5362988916afb9429d4cc790d64a6
taras.prystaiko <tarasprystaiko@gmail.com>
2024-04-19 11:07:50 +0200

    t

--- HW/HM2.3.py
+++ HW/HM2.3.py
@@ -1,5 +1,12 @@
+# from pprint import pprint
+#
+# Dictionary_output = {'Ukraine': 'Kyiv', 'Spain': 'Madrid', 'Italy': 'Rome'}
+#
+# pprint(Dictionary_output, width=1)
+
 from pprint import pprint
 
 Dictionary_output = {'Ukraine': 'Kyiv', 'Spain': 'Madrid', 'Italy': 'Rome'}
 
-pprint(Dictionary_output, width=1)
+for key, value in Dictionary_output.items():
+        print(key, ":", value)
