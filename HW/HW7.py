f2a2c5fbe729e03dafdc2dfbe58f9b7b749cbd1c
taras.prystaiko <tarasprystaiko@gmail.com>
2024-04-24 08:50:31 +0200

    t

--- HW/HW7.py
+++ HW/HW7.py
@@ -1,3 +1,4 @@
 number_of_lines =  int(input("Enter size of triangle: "))
 for line_number in range(1,number_of_lines+1):
     print(" " * (number_of_lines - line_number) + "*" * line_number)
+
---
+++ HW/HW8.py
@@ -0,0 +1,26 @@
+
+# Reading number of row
+min = 2
+row = int(input('Enter number of row: '))
+
+# Upper part of hollow diamond
+for i in range(min, row+1//2, 1):
+    for j in range(1,row-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        if j==1 or j==2*i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+# Lower part of hollow diamond
+for i in range(row-1//2, min-1, -1):
+    for j in range(1,row-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        if j==1 or j==2*i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
