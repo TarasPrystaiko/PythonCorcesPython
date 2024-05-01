07243ba5082cae6c32b475cbaf57cd4fafc31e97
taras.prystaiko <tarasprystaiko@gmail.com>
2024-05-01 06:19:48 +0200

    t

--- HW/HW11.py
+++ HW/HW11.py
@@ -1,16 +1,11 @@
 def count_words(sentences):
-    # Split the string into individual sentences
     sentence_list = [sentence.strip() for sentence in sentences.split('.') if sentence.strip()]
 
-    # Initialize an empty list to store the number of words in each sentence
     word_counts = []
 
-    # Iterate over each sentence
     for sentence in sentence_list:
-        # Split the sentence into words
         words = sentence.split()
 
-        # Count the number of words in the sentence and append to the word_counts list
         word_counts.append(len(words))
 
     return word_counts
@@ -19,4 +14,4 @@ def count_words(sentences):
 # Example usage:
 sentences = "Hello all. Here's pretty cold and hot. Choose yourself."
 result = count_words(sentences)
-print(result)  # Output: [2, 5, 2]
+print(result)
