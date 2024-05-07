29a331436ad507f623df1ed48c4b476a8fcee662
taras.prystaiko <tarasprystaiko@gmail.com>
2024-05-07 17:58:41 +0200

    t

--- HW/HW15.py
+++ HW/HW15.py
@@ -9,28 +9,29 @@
 # ...
 # sum_ = 20, product = 576
 
-def categorize_numbers(numbers):
-    divisible_by_3_not_5 = []
-    divisible_by_5_not_3 = []
-    divisible_by_both_3_and_5 = []
+def sum_and_product(lst, MIN, MAX):
+    # Initialize variables to hold the sum and product
+    sum_result = None
+    product_result = None
 
-    for num in numbers:
-        if num % 3 == 0 and num % 5 != 0:
-            divisible_by_3_not_5.append(num)
-        elif num % 5 == 0 and num % 3 != 0:
-            divisible_by_5_not_3.append(num)
-        elif num % 3 == 0 and num % 5 == 0:
-            divisible_by_both_3_and_5.append(num)
+    for num in lst:
+        if MIN <= num <= MAX:
+            if sum_result is None:
+                sum_result = 0
+                product_result = 1
 
-    return divisible_by_3_not_5, divisible_by_5_not_3, divisible_by_both_3_and_5
+            sum_result += num
+            product_result *= num
 
-# Input list
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    return sum_result, product_result
 
-# Categorize the numbers
-result = categorize_numbers(numbers)
+# Test input
+lst = [2, 4, 6, 2, 1, 1, 9, 4, 6]
+MIN = 3
+MAX = 6
 
-# Print the results
-print("elements divided  by 3", result[0])
-print("elements divided  by 5", result[1])
-print("Divisible by both 3 and 5:", result[2])
+sum_result, product_result = sum_and_product(lst, MIN, MAX)
+
+#Print
+print("Sum:", sum_result)
+print("Product:", product_result)
