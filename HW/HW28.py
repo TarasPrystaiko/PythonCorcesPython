5d3ce66afdc9dc2e4f0916402498f121fe8e8f52
taras.prystaiko <tarasprystaiko@gmail.com>
2024-05-16 09:47:15 +0200

    t

--- HW/HW28.py
+++ HW/HW28.py
@@ -2,19 +2,23 @@ import re
 
 
 def is_valid_email(email):
-    # Regular expression pattern for validating email address
     pattern = r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$'
 
-    # Match the email address with the pattern
     match = re.match(pattern, email)
 
-    # If there's a match and there's only one '@' symbol, return True
     if match and email.count('@') == 1:
         return True
     else:
         return False
 
 
-# Example usage
 email = "aaa@bbb.ccc"
-print(is_valid_email(email))  # Output: True
+email1 = "aaa@bbbccc"
+email2 = "aaabbb.ccc"
+email3 = "aaabbbccc"
+
+
+print(is_valid_email(email))
+print(is_valid_email(email1))
+print(is_valid_email(email2))
+print(is_valid_email(email3))
